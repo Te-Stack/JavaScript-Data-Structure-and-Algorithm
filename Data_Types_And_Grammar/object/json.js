df023234741328a700b580de0957dc94ab570396
Quincy Oghenetejiri <ukumakubequincy@gmail.com>
2021-07-06 23:20:22 +0100

    Data Structure and Algorithm

--- Data_Types_And_Grammar/object/json.js
+++ Data_Types_And_Grammar/object/json.js
@@ -1,5 +1,5 @@
 var a = {    val: [1,2,3],
        toJSON: function(){        return this.val.slice( 1 );    } };
-var b = {    val: [1,2,3],
+var b = {    val: [1,2,4],
     toJSON: function(){        return "[" +            this.val.slice( 1 ).join() +        "]";    } };
 JSON.stringify(a);
