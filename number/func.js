e088038021415635a43720e5c0658fe0bab65656
Quincy Oghenetejiri <ukumakubequincy@gmail.com>
2021-08-16 21:38:01 +0100

    Algorithm

--- number/func.js
+++ number/func.js
@@ -7,6 +7,13 @@ let diff = function (x, y) {
   return x - y;
 };
 
+function printA() {
+    console.log(answer);
+    var answer = 1;
+  }
+printA();
+printA();
+
 console.log(sum(10, 20))
 console.log(diff(10, 20))
 console.log(diff(6%2))
